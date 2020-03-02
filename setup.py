bd84d08aa1d45abee236410aa2c2d007a0227c58
Pasi Ollikainen <pasi.ollikainen@outlook.com>
2020-03-02 15:38:34 +0200

    Bump version: 0.4.4 → 0.4.5

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
             "add_column=merge_koskidata2primusdata.add_column:main",
         ]
     },
-    version="0.4.4",
+    version="0.4.5",
     url="https://github.com/pasiol/merge_koskidata2primusdata.git",
     license="GNU Lesser General Public License v3.0 or later (LGPLv3.0+)",
     author="Pasi Ollikainen",
