672fbfee1bf7e247d7194a06c29ba458c61e102c
Pasi Ollikainen <pasi.ollikainen@outlook.com>
2020-03-12 15:04:52 +0200

    Bump version: 0.5.1 → 0.6.0

--- merge_koskidata2primusdata/__init__.py
+++ merge_koskidata2primusdata/__init__.py
@@ -1,5 +1,5 @@
 """merge_koskidata2primusdata - Utility which merging Koski and Primus CSV reports."""
 
-__version__ = "0.5.1"
+__version__ = "0.6.0"
 __author__ = "Pasi Ollikainen <pasi.ollikainen@outlook.com>"
 
