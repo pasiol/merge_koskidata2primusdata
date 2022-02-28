e52a7d4841f41ca4e0a162f72e61d3e5fd1c9b68
Pasi Ollikainen <pasi.ollikainen@outlook.com>
2022-02-28 15:52:36 +0200

    3.10

--- setup.py
+++ setup.py
@@ -40,7 +40,6 @@ setup(
         "Development Status :: 3 - Alpha",
         "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.10",
     ],
 )
