8db0b63dfef96a26e5818bfb4a237dbab76e7539
Pasi Ollikainen <pasi.ollikainen@outlook.com>
2022-03-02 19:36:46 +0200

    Bump version: 0.9.1 → 1.0.0

--- merge_koskidata2primusdata/__init__.py
+++ merge_koskidata2primusdata/__init__.py
@@ -1,6 +1,6 @@
 """merge_koskidata2primusdata - Utility which merging Koski and Primus CSV reports."""
 
-__version__ = "0.9.1"
+__version__ = "1.0.0"
 __author__ = "Pasi Ollikainen <pasi.ollikainen@outlook.com>"
 commit = True
 tag = True
--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
             "add_column=merge_koskidata2primusdata.add_column:main",
         ]
     },
-    version="0.9.1",
+    version="1.0.0",
     url="https://github.com/pasiol/merge_koskidata2primusdata.git",
     license="GNU Lesser General Public License v3.0 or later (LGPLv3.0+)",
     author="Pasi Ollikainen",
